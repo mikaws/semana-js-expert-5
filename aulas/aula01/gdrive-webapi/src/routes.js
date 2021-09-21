c43382a8513e215994940f01393c74665be5f8b7
Michael <michaelhenriqued@gmail.com>
2021-09-20 23:27:46 -0300

    add get route with file helper

--- aulas/aula01/gdrive-webapi/src/routes.js
+++ aulas/aula01/gdrive-webapi/src/routes.js
@@ -1,35 +1,44 @@
+import FileHelper from "./fileHelper.js"
 import { logger } from "./logger.js"
+import { dirname, resolve } from 'path'
+import {  fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const defaultDownloadsFolder = resolve(__dirname, '../', "downloads")
 
 export default class Routes {
     io
-    constructor() {
-
+    constructor(downloadsFolder = defaultDownloadsFolder) {
+        this.downloadsFolder = downloadsFolder
+        this.fileHelper = FileHelper
     }
 
-    setSocketInstance(io){
+    setSocketInstance(io) {
         this.io = io
     }
 
-    async defaultRoute(request, response){
+    async defaultRoute(request, response) {
         response.end('hello world')
     }
 
-    async options(request, response){
+    async options(request, response) {
         response.writeHead(204)
-        response.end('hello world')
+        response.end()
     }
 
-    async post(request, response){
+    async post(request, response) {
         logger.info('post')
         response.end()
     }
 
-    async get(request, response){
-        logger.info('get')
-        response.end()
+    async get(request, response) {
+        const files = await this.fileHelper.getFilesStatus(this.downloadsFolder)
+
+        response.writeHead(200)
+        response.end(JSON.stringify(files))
     }
 
-    handler(request, response){
+    handler(request, response) {
         response.setHeader('Access-Control-Allow-Origin', '*')
         const chosen = this[request.method.toLowerCase()] || this.defaultRoute
 

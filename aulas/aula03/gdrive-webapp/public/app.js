1ac6dea9baa06095e7b73911844d32901120b02c
Michael <michaelhenriqued@gmail.com>
2021-10-02 16:00:57 -0300

    add connection with api

--- aulas/aula03/gdrive-webapp/public/app.js
+++ aulas/aula03/gdrive-webapp/public/app.js
@@ -1,1 +1,16 @@
-console.debug('hello world!')
+import AppController from "./src/appController.js";
+import ConnectionManager from "./src/connectionManager.js";
+
+const API_URL = "https://localhost:3000"
+
+const appController = new AppController({
+    connectionManager: new ConnectionManager({
+        apiUrl: API_URL
+    })
+})
+
+try {
+    await appController.initialize()
+} catch (error) {
+    console.error('error on initializing', error)
+}
